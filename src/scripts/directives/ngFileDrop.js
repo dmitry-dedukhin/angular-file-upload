977fdd145c2aed891cb645a8ce6db31df35b99a1
Dmitry Dedukhin <ddedukhin@asdco.ru>
2014-07-16 14:20:22 +0400

    Check that files really present in dragging data

--- src/scripts/directives/ngFileDrop.js
+++ src/scripts/directives/ngFileDrop.js
@@ -27,10 +27,21 @@ app.directive('ngFileDrop', ['$fileUploader', function ($fileUploader) {
                         event.dataTransfer :
                         event.originalEvent.dataTransfer; // jQuery fix;
 
-                    event.preventDefault();
-                    event.stopPropagation();
-                    dataTransfer.dropEffect = 'copy';
-                    scope.$broadcast('file:addoverclass');
+                    var hasFiles = true;
+                    if(dataTransfer.types) {
+                        if(dataTransfer.types.contains && !dataTransfer.types.contains('Files')) {
+                            hasFiles = false;
+                        }
+                        if(dataTransfer.types.indexOf && dataTransfer.types.indexOf('Files') === -1) {
+                            hasFiles = false;
+                        }
+                    }
+                    if(hasFiles) {
+                        event.preventDefault();
+                        event.stopPropagation();
+                        dataTransfer.dropEffect = 'copy';
+                        scope.$broadcast('file:addoverclass');
+                    }
                 })
                 .bind('dragleave', function (event) {
                     if (event.target === element[0]) {
